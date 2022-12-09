4fc4b429e0c4992cc353bd9a5f1933762b806826
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2022-12-09 10:05:53 -0500

    segunda tarea

---
+++ tareados.js
@@ -0,0 +1,9 @@
+const mifecha=new Date(1985,2,11);
+const desarrollador = true;
+const libro_favorito = {
+    titulo:"La vuelta al mundo en 80 días",
+    autor:"Julio Verne",
+    fecha:mifecha,
+    url:"https://es.wikipedia.org/wiki/La_vuelta_al_mundo_en_ochenta_d%C3%ADas",
+};
+const milista=["Andrés Felipe", 36, desarrollador, mifecha, libro_favorito];
